db19df2e165183ffb7430bc2f20648cb64c67e74
mechaniccoder <yuseunghwan94@gmail.com>
2021-06-30 16:12:20 +0900

    Setup and tear down

---
+++ setup.test.js
@@ -0,0 +1,30 @@
+const initialDatabase = () => {}
+const clearDatabase = () => {}
+
+// 각 테스트 전에 실행
+beforeEach(() => {
+  initialDatabase();
+})
+
+// 각 테스트 후에 실행
+afterEach(() => {
+  clearDatabase();
+})
+
+// 테스트마다 재사용하고 싶을때
+beforeAll(() => {
+  initialDatabase();
+})
+
+afterAll(() => {
+  clearDatabase();
+})
+
+describe('it can apply each test', () => {
+  beforeEach(() => {
+    initialDatabase();
+  })
+  it('boilerplate', () => {
+    expect(true).toBeTruthy();
+  })
+})
